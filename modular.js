41ec8187e97164831d865913236160e6712c987e
gyaresu <me@gareth.com.au>
2013-09-22 22:54:12 +0200

    returned an error on its callback - MAKE IT MODULAR

--- modular.js
+++ modular.js
@@ -1,4 +1,4 @@
 var filter = require('./filter.js')
-filter(process.argv[2], process.argv[3], function () {
-  if (err) throw err;
+filter(process.argv[2], process.argv[3], function (err) {
+  console.log(err);
 });
