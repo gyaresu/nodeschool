1f99f90fa4278603688742067a9929150a4fb40c
gyaresu <me@gareth.com.au>
2013-10-01 21:15:05 +0200

    added api.js

---
+++ api.js
@@ -0,0 +1,16 @@
+var http = require('http')
+var url = require('url')
+
+var server = http.createServer(function (req, res) {
+  if (req.url.parse[pathname] == '/api/parsetime')
+    res.writeHead(200, { 'Content-Type': 'application:json' })
+    res.end(req.url.pathname)
+
+  if (req.url.toString() == '/api/unixtime')
+    console.log('unixtime')
+    
+  res.end()
+
+})
+
+server.listen(8000)
