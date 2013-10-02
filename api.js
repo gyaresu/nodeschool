0ed6f490899707c99d9f9b45143a5abeae876593
gyaresu <me@gareth.com.au>
2013-10-02 18:16:05 +0200

    bump

--- api.js
+++ api.js
@@ -2,14 +2,14 @@ var http = require('http')
 var url = require('url')
 
 var server = http.createServer(function (req, res) {
-  if (req.url.parse[pathname] == '/api/parsetime')
+  if (req.url.parse == '/api/parsetime')
     res.writeHead(200, { 'Content-Type': 'application:json' })
     res.end(req.url.pathname)
 
   if (req.url.toString() == '/api/unixtime')
     console.log('unixtime')
     
-  res.end()
+  res.end("end")
 
 })
 
