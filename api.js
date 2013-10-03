ff68412ef0835449853d76bbe480657f538f213b
gyaresu <me@gareth.com.au>
2013-10-03 19:06:14 +0200

    end thursday office

--- api.js
+++ api.js
@@ -1,16 +1,20 @@
-var http = require('http')
-var url = require('url')
+var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function (req, res) {
-  if (req.url.parse == '/api/parsetime')
-    res.writeHead(200, { 'Content-Type': 'application:json' })
-    res.end(req.url.pathname)
+  if (req.url == '/api/parsetime')
+    // works: console.log("Got the request url for parsetme");
+    res.writeHead(200, { 'Content-Type': 'application:json' });
+    res.end("Parsetime: " + req.url);
 
-  if (req.url.toString() == '/api/unixtime')
-    console.log('unixtime')
-    
-  res.end("end")
+  if (req.url == '/api/unixtime')
+    res.writeHead(200, { 'Content-Type': 'application:json' });
+    res.end("Unixtime: " + req.url);
+    //
+  //res.end("end")
+  //res.end("Thing: " + req.headers);
+  console.log("grabbed url: " + url.parse(req.url, true));
 
-})
+});
 
-server.listen(8000)
+server.listen(8000);
