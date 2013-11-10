af05f879600e481ff0eb0a964593a1c74c03569c
gyaresu <me@gareth.com.au>
2013-11-10 01:37:08 +0100

    Basics PUT completed

--- basics_put.js
+++ basics_put.js
@@ -1,9 +1,12 @@
 var level = require('level')
 var db = level(process.argv[2])
+var string = process.argv[3]
+var p = JSON.parse(string) // should be a JSON encoded string
 
-string = JSON.parse(process.argv[3]) // should be a JSON encoded string
-
-for (var i = 0; i < string.length; i++)
-  console.log(string[i])
+for (var key in p) {
+  if (p.hasOwnProperty(key)) {
+    db.put(key, p[key])
+  }
+}
 
 
