23624b2e94294dc0603538b10f7bf3f28f654db0
gyaresu <me@gareth.com.au>
2014-03-30 01:09:07 +0100

    bytewiser - line splitter: Almost correct except for the last two lines. No idea...

---
+++ bytewiser/line_splitter.js
@@ -0,0 +1,15 @@
+//DEC OCT HEX BIN       Symbol  Description
+//92  134 5C  01011100  \       Backslash
+var fs = require('fs');
+var temp = 0;
+
+fs.readFile(process.argv[2], function (err, data) {
+  if (err) throw err;
+  for (var i = 0; i < data.length; i++) {
+    if (data[i] === 10) {
+      console.log(data.slice(temp,i));
+      i++;
+      temp = i;
+    }
+  }
+});
