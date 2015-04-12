905761ddbd90331e08394bc57ae3aae8c5865745
gyaresu <me@gareth.com.au>
2015-04-11 22:30:14 -0700

    8 of 10 planet proto

--- planetproto/020.js
+++ planetproto/020.js
@@ -1,25 +1,29 @@
 // -> Create a machine object
 //    with a property motors = 4
-__
+var machine = {
+	motors: 4
+};
 
 // -> Create a robot object
 //    with a property friendly = true
-__
+var robot = {
+	friendly: true
+};
 
 // -> Create a robby object
-__
+var robby = {};
 
 // -> Make machine the prototype of robot
-__
+robot.__proto__ = machine 
 
 // -> Make robot the prototype of robby
-__
+robby.__proto__ = robot
 
 // -> What is `robby.motors`?
-claim(robby.motors, __);
+claim(robby.motors, 4);
 
 // -> What is `robby.friendly`?
-claim(robby.friendly, __);
+claim(robby.friendly, true);
 
 
 // ------------------------------------------------
