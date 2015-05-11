ca1b3cca21c01cb0df12d3e089b2de28a88aceed
gyaresu <me@gareth.com.au>
2015-05-11 14:25:49 -0700

    basic recursion solved

--- functional-javascript/basic-recursion/basic-recursion.js
+++ functional-javascript/basic-recursion/basic-recursion.js
@@ -1,17 +1,5 @@
-//function reduce(arr, fn, initial) {
-//	if (!arr.length) return initial;
-//    var item = arr.pop(-1);
-//    initial[item] = ++initial[item] || 1;
-//    //  console.log(initial, item, initial[item]);
-//    return reduce(arr.slice(1), fn, initial);
-//}
-//
-//module.exports = reduce;
-
-
 module.exports = function reduce(arr, fn, initial) {    //# setup function w/target, operator, and initial value
    if (arr.length === 0) {return initial;}              //# base case, does not recurse
    initial = fn(initial, arr[0]);                       //# sets init-OP-first result as new init
-   console.log(initial);
    return reduce(arr.slice(1), fn, initial);            //# rightshifts first so arr[0] index is the new current value
 };
