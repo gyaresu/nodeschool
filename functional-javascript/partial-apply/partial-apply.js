c6514b1605e61a2c963a286c2a6e96aeba27782c
gyaresu <me@gareth.com.au>
2015-05-11 21:05:49 -0700

    partial-apply complete

--- functional-javascript/partial-apply/partial-apply.js
+++ functional-javascript/partial-apply/partial-apply.js
@@ -1,7 +1,22 @@
 var slice = Array.prototype.slice;
 
 function logger(namespace) {
-    //
+    return function(args) {
+        //console.log(slice.call(arguments).join(' '));
+        return console.log(namespace + ' ' + slice.call(arguments).join(' '));
+    };
 }
 
 module.exports = logger;
+
+// Official Solution:
+
+// var slice = Array.prototype.slice
+
+// function logger(namespace) {
+//   return function() {
+//     console.log.apply(console, [namespace].concat(slice.call(arguments)))
+//   }
+// }
+
+// module.exports = logger
