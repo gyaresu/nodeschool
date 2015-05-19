6d8ecffc84e12fa7c862a4fbb4b3f5516dfdd4b4
gyaresu <me@gareth.com.au>
2015-05-19 16:29:56 -0700

    finished workshopper

--- count-to-6/9-tagged-template-strings.js
+++ count-to-6/9-tagged-template-strings.js
@@ -1,29 +1,18 @@
-// ' | &apos;
-// " | &quot;
-// < | &lt;
-// > | &gt;
-// & | &amp;
+let fix = (a) => {
 
-module.exports = (username, comment) => {
-
-    let fix = (text) => {
-        switch(text){
-            case "'":
-                return '&apos;';
-            case '"':
-                return '&quot;';
-            case '<':
-                return '&lt;';
-            case '>':
-                return '&gt;';
-            case '&':
-                return '&amp;';
-            default:
-                break;
-        }
-    };
-
-    let html = (content, fn) => 
-    console.log(html`<b>${ username } says</b>: "${ comment }"`);
+    return a.replace(/&/g, '&amp;')
+            .replace(/>/g, '&gt;')
+            .replace(/</g, '&lt;')
+            .replace(/'/g, '&apos;')
+            .replace(/"/g, '&quot;');
 };
 
+let html = (wrapping, ...args) => {
+    let result = wrapping[0];
+    for (let i = 0; i < args.length; ++i) {
+        result += fix(args[i]) + wrapping[i+1];
+    }
+    return result;
+};
+
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
