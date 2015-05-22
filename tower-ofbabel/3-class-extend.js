a3a23313c2b39df5722fecc0dc7598860f6b1342
gyaresu <me@gareth.com.au>
2015-05-21 23:03:53 -0700

    3 Class Extend complete

--- tower-ofbabel/3-class-extend.js
+++ tower-ofbabel/3-class-extend.js
@@ -1,81 +1,43 @@
+'use strict';
+
 class Character {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.health_ = 100;
-  }
-  attack(character) {
-    if (character !instanceof Character) throw new Error('');
-    character.health_ -= 10;
-  }
-}
-
-class Monster extends Character {
-  constructor(x, y, name) {
-    super(x, y);
-    this.name = name;
-  }
-}
-
-class Monster extends Character {
-  constructor(x, y, name) {
-    super(x, y);
-    this.name = name;
-  }
-  attack(character) {
-    super.attack(character);
-    character.health_ -= 5;
-  }
-}
-
-/* Above here be dragons */
-
-var util = require('util');
-
-class Character {
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
-    this.health_ = 100;
   };
   damage() {
-    super(x,y);
-    this.health_ - 10;
-  }
+    this.health_ -= 10;
+  };
+  getHealth() {
+    return this.health_;
+  };
+  toString() {
+    return 'x: ' + this.x + ' y: ' + this.y + ' health: ' + this.getHealth();
+  };
 }
-/* Below here be dragons */
 
-var util = require('util');
-var Character = function(x, y) {
-  this.x = x;
-  this.y = y;
-  this.health_ = 100;
-};
+class Player extends Character {
+  constructor(x, y, name) {
+    super(x, y);
+    this.name = name;
+  };
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  };
+  toString() {
+    return 'name: ' + this.name + ' ' + super.toString();
+  };
+}
 
-Character.prototype.damage = function() {
-  this.health_ = this.health_ - 10;
-};
-
-Character.prototype.getHealth = function() {
-  return this.health_;
-};
-
-Character.prototype.toString = function() {
-  return "x: " + this.x + " y: " + this.y + " health: " + this.getHealth();
-};
-
-var Player = function(x, y, name) {
-  Character.call(this, x, y);
-  this.name = name;
-};
-
-util.inherits(Player, Character);
-
-Player.prototype.move = function(dx, dy){
-  this.x += dx;
-  this.y += dy;
-};
-
-Player.prototype.toString = function() {
-  return "name: " + this.name + " " + Player.super_.prototype.toString.call(this);
-};
+var x = process.argv[2];
+var y = process.argv[3];
+var name = process.argv[4];
+var character = new Character(+x, +y);
+character.damage();
+console.log(character.toString());
+var player = new Player(+x, +y, name);
+player.damage();
+player.move(7, 8);
+console.log(player.toString());
