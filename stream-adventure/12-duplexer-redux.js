a669d749ef907c77925386ada9cf67f48efa392a
Gareth <me@gareth.com.au>
2015-05-26 16:35:44 -0700

    stream-adventure 12 - duplexer redux

---
+++ stream-adventure/12-duplexer-redux.js
@@ -0,0 +1,20 @@
+var duplexer = require('duplexer2')
+var through = require('through2')
+
+module.exports = function (counter) {
+  var count = {}
+
+  function transform (chunk, enc, callback) {
+    count[chunk.country] = (count[chunk.country] || 0) + 1
+    callback()
+  }
+
+  function end(done) {
+    counter.setCounts(count)
+    done()
+  }
+
+  var work = through.obj(transform, end)
+  return duplexer(work, counter)
+
+}
--- stream-adventure/duplexer-redux.js
+++
@@ -1,7 +0,0 @@
-var duplexer = require('duplexer2')
-var stream = require('stream')
-
-module.exports = function (counter) {
-  var count = stream().Readable
-  return duplexer({thing: 'stuff'}, countries, count)
-}
