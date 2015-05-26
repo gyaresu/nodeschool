08d49e4cdd7a1f9ce22e36ad977383322dc29780
Gareth <me@gareth.com.au>
2015-05-25 22:45:22 -0700

    stream-adventure - working on duplexer redux

---
+++ stream-adventure/duplexer-redux.js
@@ -0,0 +1,7 @@
+var duplexer = require('duplexer2')
+var stream = require('stream')
+
+module.exports = function (counter) {
+  var count = stream().Readable
+  return duplexer({thing: 'stuff'}, countries, count)
+}
