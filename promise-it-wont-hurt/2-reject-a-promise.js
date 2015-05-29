7793fb57aeb6819b5fd7c869bdb95c259197f0e2
Gareth <me@gareth.com.au>
2015-05-29 03:31:05 -0700

    promise-it-wont-hurt - to reject or not to reject. Works but looks wrong, will check tomorrow

---
+++ promise-it-wont-hurt/2-reject-a-promise.js
@@ -0,0 +1,10 @@
+var q = require('q')
+var defer = q.defer()
+
+function printError(err) {
+  console.log(err.message)
+}
+
+defer.promise.then(null, printError)
+
+setTimeout(defer.reject, 300, new Error('I FIRED'))
