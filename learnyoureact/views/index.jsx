7f2401042c68245f9a83be3ad12f3ed1edaf06e2
Gareth <me@gareth.codes>
2015-06-09 01:13:23 -0700

    learnyoureact - mutable state

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -34,9 +34,18 @@ var Todo = React.createClass({
   propTypes: {
     title: React.PropTypes.string.isRequired
   },
+  getInitialState: function () {
+    return {
+      checked: this.props.checked || true
+    }
+  },
+  handleChange: function (e) {
+    this.setState({checked: e.target.checked})
+  },
   render: function () {
     return (
       <tr>
+        <td style={{border: "1px solid black"}}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
         <td style={{border: "1px solid black"}}>{this.props.title}</td>
         <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
