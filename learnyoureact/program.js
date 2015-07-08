c5e107274622293b0656d9d8563fd5cab39e135f
Gareth <me@gareth.codes>
2015-07-08 14:28:55 -0700

    learnyoureact - props from server completed

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -9,8 +9,11 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 require('node-jsx').install()
 
+// Simulated data: This is where you'd call from a database
+var data = [{title: 'Shopping', detail: process.argv[3]}, {title: 'Hair cut', detail: process.argv[4]}]
+
 app.use('/', function (req, res) {
-  res.render('index', '')
+  res.render('index', {data: data})
 })
 
 app.listen(app.get('port'), function () {})
