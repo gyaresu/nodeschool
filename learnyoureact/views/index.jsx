77960cea866472a43c4b22196c4281269d112bb1
Gareth <me@gareth.codes>
2015-07-08 13:39:57 -0700

    learnyounode - props completed

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -10,16 +10,32 @@ var TodoBox = React.createClass({
       </div>
     );
   }
-});
+})
 
 var TodoList = React.createClass({
-  render: function () {
+  render: function() {
     return (
       <div className = 'todoList'>
-        I am a TodoList.
+        <table style={{border: '2px solid black'}}>
+          <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+          </tbody>
+        </table>
       </div>
-    )
+    );
   }
+});
+
+var Todo = React.createClass({
+  render: function () {
+    return (
+      <tr>
+        <td style={{border: '1px solid black'}}>{this.props.title}</td>
+        <td style={{border: '1px solid black'}}>{this.props.children}</td>
+      </tr>
+    )
+  }  
 });
 
 var TodoForm = React.createClass({
@@ -30,6 +46,6 @@ var TodoForm = React.createClass({
       </div>
     )
   }
-});
+})
 
 module.exports = TodoBox;
