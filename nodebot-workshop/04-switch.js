03049451ab6c732aa963054b58a0265761aff868
Gareth <me@gareth.codes>
2015-07-14 01:52:27 -0700

    nodebot-workshop - switch

---
+++ nodebot-workshop/04-switch.js
@@ -0,0 +1,13 @@
+var five = require('johnny-five')
+
+var board = new five.Board()
+
+board.on('ready', function () {
+
+  var button = new five.Button(5)
+  var led = new five.Led(9)
+
+  button.on('press', function () {
+    led.toggle()
+  })
+})
