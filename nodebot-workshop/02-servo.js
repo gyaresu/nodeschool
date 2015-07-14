13f8d7500d52d038d2d1e1e4bf599f083be88ed9
Gareth <me@gareth.codes>
2015-07-14 01:50:11 -0700

    nodebot-workshop - motor

---
+++ nodebot-workshop/02-servo.js
@@ -0,0 +1,10 @@
+var five = require('johnny-five')
+var board = new five.Board()
+
+board.on('ready', function () {
+  var servo = new five.Servo(9)
+  servo.sweep()
+  board.wait(3000, function () {
+    servo.stop().center()
+  })
+})
