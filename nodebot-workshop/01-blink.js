3ddf22e1e827995cda3f7219b845456596c95cf7
Gareth <me@gareth.codes>
2015-11-21 14:52:48 -0800

    nodeschool nov

--- nodebot-workshop/01-blink.js
+++ nodebot-workshop/01-blink.js
@@ -3,14 +3,14 @@ var board = new five.Board()
 
 board.on('ready', function () {
   var led1 = new five.Led(13)
-  var led2 = new five.Led(12)
-  var led3 = new five.Led(14)
-  led.strobe(1000)
+  //var led2 = new five.Led(12)
+  //var led3 = new five.Led(14)
+  led1.strobe(1000)
 
   board.repl.inject({
     led1: led1,
-    led2: led2,
-    led3: led3
+    //led2: led2,
+    //led3: led3
   })
 
 })
