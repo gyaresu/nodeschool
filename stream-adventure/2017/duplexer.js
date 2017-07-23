5beb8afe74babce2caa4cc9b677e8e04cc50519f
Gareth <me@gareth.codes>
2017-07-22 18:46:40 -0700

    duplexing process streams

--- stream-adventure/2017/duplexer.js
+++ stream-adventure/2017/duplexer.js
@@ -1,1 +1,15 @@
 // Stream Adventure
+// duplexer.js
+
+var duplexer = require('duplexer2')
+var cp = require('child_process')
+
+
+
+// function with command string and args array 
+module.exports = function (cmd, args) {
+  // duplex the streams
+  var proc = cp.spawn(cmd, args)
+  var streams = duplexer(proc.stdin, proc.stdout)
+  return streams
+}
