aa376c825fb79fb637d3b8a7566b60a17bfa6f45
Gareth <me@gareth.codes>
2017-08-14 14:18:58 -0700

    functional-javascript - commented on callback in recursion

--- functional-javascript/2017/07-recursion.js
+++ functional-javascript/2017/07-recursion.js
@@ -1,6 +1,10 @@
 function reduce(arr, fn, initial) {
   if (!arr.length) return initial
+
+  // Callback passed to this function looks like
+  // cb(accumulator, currentValue, currentIndex, array) 
   fn(initial, arr[0], 0, arr)
+
   return reduce(arr.slice(1), fn, initial)
 }
 
