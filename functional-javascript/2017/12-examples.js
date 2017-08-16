9ee5cf7f4887c6e35857d38a867ab8264e70d47f
Gareth <me@gareth.codes>
2017-08-16 14:56:45 -0700

    more playing around with pass by ref vs value

--- functional-javascript/2017/12-examples.js
+++ functional-javascript/2017/12-examples.js
@@ -1,22 +1,44 @@
-function noChange (ref) {
-  ref = {}
+var thing2 = { key: 'start'}
+var thing3 = { key: 'start'}
 
-  console.log('Call the noChange() function: ', ref, a)
+function test () {
+  function noChange (ref) {
+    ref = {}
+    console.log('Call the noChange() function. Now `ref` =', ref, ' and `a` =', a)
+  }
+
+  function change (ref) {
+    ref.key = 'changed'
+    console.log('Call the change() function: Now `ref` =', ref, ' and `a` =', a)
+  }
+
+  a = { key: 'initial'}
+  noChange(a)
+  console.log('What\'s `a` look like after calling noChange()?: ', a)
+  change(a)
+  console.log('What\'s `a` look like after calling change()?: ', a)
+
+  // λ node 12-examples.js
+  // Call the noChange() function. Now ref =  {}  and `a` =  { key: 'initial' }
+  // What's `a` look like after calling noChange()?:  { key: 'initial' }
+  // Call the change() function: Now ref =  { key: 'changed' }  and `a` =  { key: 'changed' }
+  // What's `a` look like after calling change()?:  { key: 'changed' }
 }
 
-function change (ref) {
-  ref.key = 'changed'
-  console.log('Call the change() function: ', ref, a)
+// test()
+
+// test2
+function test2 () {
+  thing2.key = 'thing2'
 }
+test2()
+console.log(thing2.key)
 
-a = { key: 'initial'}
-noChange(a)
-console.log('What\'s `a` look like now?: ', a)
-change(a)
-console.log('What\'s `a` look like now?: ', a)
+// test3
+function test3 (obj) {
+  obj = { key: 'test3'}
+  console.log(obj.key)
+}
+test3(thing3)
+console.log(thing3.key)
 
-// λ node 12-examples.js
-// Call the noChange() function:  {} { key: 'initial' }
-// What's `a` look like now?:  { key: 'initial' }
-// Call the change() function:  { key: 'changed' } { key: 'changed' }
-// What's `a` look like now?:  { key: 'changed' }
